d2e2de3125a653b2d46633e924fbef33b7ca1754
David <David.thiman@gmail.com>
2020-10-12 15:02:07 +0200

    Testing in the new you

--- src/main/java/com/zanatech/zanatech/Controllers/ProductController.java
+++ src/main/java/com/zanatech/zanatech/Controllers/ProductController.java
@@ -1,9 +1,20 @@
 package com.zanatech.zanatech.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
 
+    @RequestMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+
+    @RequestMapping("/url")
+    public String name(){
+        return "null";
+    }
 
 }
