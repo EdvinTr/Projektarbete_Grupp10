8b09384bf5cc028f9d53ab26a9d3bb09d2eccba9
David <David.thiman@gmail.com>
2020-10-12 14:18:27 +0200

    Test

--- src/main/java/com/zanatech/zanatech/Controllers/ProductController.java
+++ src/main/java/com/zanatech/zanatech/Controllers/ProductController.java
@@ -1,7 +1,15 @@
 package com.zanatech.zanatech.Controllers;
 
-public class ProductController {
-    //HEllo world
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-    //Vem vare som kasta!
+@Controller
+public class ProductController {
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("Value", "Nein!");
+        return "index";
+    }
 }
