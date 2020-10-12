1d2f164ac4b21a42fae605b5241a0dfcbbfd4e2a
David <David.thiman@gmail.com>
2020-10-12 13:13:07 +0200

    init commit

---
+++ src/main/java/com/zanatech/zanatech/ZanatechApplication.java
@@ -0,0 +1,13 @@
+package com.zanatech.zanatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZanatechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZanatechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zanatech/zanatech/ZanatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanatech.zanatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZanatechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
