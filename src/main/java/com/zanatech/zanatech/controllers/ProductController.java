34bb49446ad569642b0d41cc2c0e50af91d7d14f
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2020-10-13 10:47:25 +0200

    add spring security

--- src/main/java/com/zanatech/zanatech/controllers/ProductController.java
+++ src/main/java/com/zanatech/zanatech/controllers/ProductController.java
@@ -5,8 +5,10 @@ import com.zanatech.zanatech.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -16,7 +18,8 @@ public class ProductController {
     private ProductService service;
     private Object Product;
 
-    @RequestMapping("/zanatech")
+
+    @RequestMapping("/")
     public String viewHomePage(Model model) {
         List<Products> listProducts = service.listAll();
         Products products = new Products();
