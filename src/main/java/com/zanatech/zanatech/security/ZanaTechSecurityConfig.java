76ec3402b7db0f85f460ff6ff481b4800f96ef47
Robin Hellström <robin1robin@hotmail.com>
2020-10-15 15:20:59 +0200

    Login design COMPLETE YO!

--- src/main/java/com/zanatech/zanatech/security/ZanaTechSecurityConfig.java
+++ src/main/java/com/zanatech/zanatech/security/ZanaTechSecurityConfig.java
@@ -1,7 +1,8 @@
-package com.zanatech.zanatech;
+package com.zanatech.zanatech.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -14,7 +15,7 @@ public class ZanaTechSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         	http
             .csrf().disable()
-            .authorizeRequests().antMatchers("/login").permitAll()
+            .authorizeRequests().antMatchers("/login", "/css/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin()
